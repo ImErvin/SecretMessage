4fe9127a644c0eeb591334cea22b4436010b51fa
Ervin Mamutov <Ervii@DESKTOP-88S3MQI.(none)>
2016-11-22 14:14:13 +0000

    Educated decision to use CouchDB again. Set up CouchDB and print the message from the database to a html template

--- webapp.py
+++ webapp.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import json
-from flask_cors import CORS, cross_origin
-from flask_pymongo import PyMongo
+import couchdb
 from bson.json_util import dumps
 import flask as flaskApp
 
 app = Flask(__name__)
-mongo = PyMongo(app)
-CORS(app)
+couch = couchdb.Server("http://127.0.0.1:5984")
+db = couch['secretmessage']
 
 @app.route('/') 
 def root():
@@ -17,17 +16,18 @@ def root():
 
 @app.route('/<messageId>', methods=['GET'])
 def message(messageId):
-    data = mongo.db.test.find()
-    print(data)
-    [doc for doc in data]
-    print(doc)
+    for id in db:
+        if(id == messageId):
+            doc = db[id]
     
-    return render_template('messageTemplate.html', messageIdo = dumps(data))
+    return render_template('messageTemplate.html', messageIdo = doc['message'])
 
 @app.route('/generateUrl', methods=['GET'])
 def generateUrl():
+    
 	url = flaskApp.request.values["userinput"]
 	return 'http://127.0.0.1:5000/' + url
 
-if __name__ == "__main__":     
-	app.run()
+if __name__ == "__main__":  
+    
+    app.run()
